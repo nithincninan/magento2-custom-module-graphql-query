fafbb0de90fa81d5f9cd06f62b2d7e4dca09210d
chackoni <chackoni@adobe.com>
2021-06-27 23:31:36 +0530

    m2.4 custom-module-query

---
+++ Magento/BrandGraphQl/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © 2021 MDC_NCN, LLC. All rights reserved.
+ */
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Magento_BrandGraphQl', __DIR__);
